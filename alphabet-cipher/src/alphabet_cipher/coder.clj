8e989634569959b17e630f1cb61d87bd04157baa
Jonas Bergman <jonkjonas@gmail.com>
2016-02-29 15:41:45 +0100

    Week 4 Day 1, encoding works

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,36 @@
 (ns alphabet-cipher.coder)
 
+(def letters "abcdefghijklmnopqrstuvwxyz")
+
+;; Turn all letters to lowercase. 
+;; No checking, meant to be used here for a-z,A-Z
+(defn lower-case-char [ch]
+  (if (> (int \a) (int ch))
+    (char (+ (int \a) 
+             (- (int ch) (int \A))))
+    ch))
+
+;; Generate a string of all lowercase letters, starting at ch
+;; and wrapping from 'z' to 'a'
+(defn generate-string-for-char [ch]
+  (let [n (.indexOf letters (str ch))]
+    (apply str (concat (drop n letters) (take n letters)))))
+
+;; Generate a string of the correct length by repeating the keyword
+;; if necessary.
+(defn generate-key-string [kw len]
+  (let [reps (int (Math/ceil (/ len (count kw))))] 
+    (apply str (take len (apply str (repeat reps kw))))))
+
+;; Encode a single char m using k as encoding key
+(defn encode-char [k m]
+  (get (generate-string-for-char m) (.indexOf letters (str k))))
+
+;; Encode a string using keyword
 (defn encode [keyword message]
-  "encodeme")
+  (apply str (map encode-char 
+                  (generate-key-string keyword (count message)) 
+                  message)))
 
 (defn decode [keyword message]
   "decodeme")
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -2,6 +2,27 @@
   (:require [clojure.test :refer :all]
             [alphabet-cipher.coder :refer :all]))
 
+(deftest test-generate-string
+  (testing "produce a string starting on character"
+    (is (= "abcdefghijklmnopqrstuvwxyz"
+           (generate-string-for-char \a)))
+    (is ( = "qrstuvwxyzabcdefghijklmnop"
+           (generate-string-for-char \q)))))
+
+(deftest test-lower-case-char
+  (testing "convert a char to lower case"
+    (is (= \a (lower-case-char \A)))
+    (is (= \b (lower-case-char \b)))))
+
+(deftest test-key-string 
+  (testing "generate a string of a length from the keyword"
+    (is (= "sconessconessco"
+           (generate-key-string "scones" (count "meetmebythetree"))))))
+
+(deftest test-encode-char
+  (testing "encoding a single char"
+    (is (= \e (encode-char \s \m)))))
+
 (deftest test-encode
   (testing "can encode given a secret keyword"
     (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
@@ -9,16 +30,16 @@
     (is (= "egsgqwtahuiljgs"
            (encode "scones" "meetmebythetree")))))
 
-(deftest test-decode
-  (testing "can decode an cyrpted message given a secret keyword"
-    (is (= "meetmeontuesdayeveningatseven"
-           (decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")))
-    (is (= "meetmebythetree"
-           (decode "scones" "egsgqwtahuiljgs")))))
+;(deftest test-decode
+  ;(testing "can decode an cyrpted message given a secret keyword"
+    ;(is (= "meetmeontuesdayeveningatseven"
+           ;(decode "vigilance" "hmkbxebpxpmyllyrxiiqtoltfgzzv")))
+    ;(is (= "meetmebythetree"
+           ;(decode "scones" "egsgqwtahuiljgs")))))
 
-(deftest test-decipher
-  (testing "can extract the secret keyword given an encrypted message and the original message"
-    (is (= "vigilance"
-           (decipher "opkyfipmfmwcvqoklyhxywgeecpvhelzg" "thequickbrownfoxjumpsoveralazydog")))
-    (is (= "scones"
-           (decipher "hcqxqqtqljmlzhwiivgbsapaiwcenmyu" "packmyboxwithfivedozenliquorjugs")))))
+;(deftest test-decipher
+  ;(testing "can extract the secret keyword given an encrypted message and the original message"
+    ;(is (= "vigilance"
+           ;(decipher "opkyfipmfmwcvqoklyhxywgeecpvhelzg" "thequickbrownfoxjumpsoveralazydog")))
+    ;(is (= "scones"
+           ;(decipher "hcqxqqtqljmlzhwiivgbsapaiwcenmyu" "packmyboxwithfivedozenliquorjugs")))))
